314c19093d451746f9b586ac657ca7741a0eeb6f
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-11-10 21:22:36 +0700

    feat: add app.use for express json

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const authUserRouter = require('./app/routes/authUserRoute');
 
+app.use(express.json());
 app.use('/authuser', authUserRouter);
 
 app.listen(3000, () => {
