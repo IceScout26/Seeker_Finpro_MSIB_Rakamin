4a20da8b2b091957768c6e0ac0afc556217494bb
IceScout26 <dhimasscout@gmail.com>
2023-11-10 17:44:17 +0700

    feat : scripts for seeding using npm run seeding

--- db/seeding.js
+++ db/seeding.js
@@ -3,7 +3,10 @@ var fs = require("fs");
 
 const seedQuery = fs.readFileSync("db/seeding.sql", { encoding: "utf8" });
 pool.query(seedQuery, (err, res) => {
-  console.log(err);
-  console.log("seeding done");
+  if (err) {
+    console.error("Error executing seeding query:", err.message);
+  } else {
+    console.log("Seeding done");
+  }
   pool.end();
 });
