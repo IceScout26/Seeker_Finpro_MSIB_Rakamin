31b0b0e1dbfcdff35efb6454e56556b58a753703
Poodoop <dickyaristyo43@gmail.com>
2023-11-14 02:41:46 +0700

    Chore: test register and login with dotenv in entry point

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 const authUserRouter = require('./app/routes/authUserRoute');
 
+//require('dotenv').config(); sudah dites dalam index.js, nggak bisa bekerja
+
+const app = express();
+
 app.use(express.json());
+
 app.use('/authuser', authUserRouter);
 
 app.listen(3000, () => {
