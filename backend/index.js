aeeb474bb02dd5805b8ce9f2a18fd75fc57555f3
IceScout26 <dhimasscout@gmail.com>
2023-11-14 20:44:23 +0700

    feat : skill router

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authUserRouter = require('./app/routes/authUserRoute');
+const skillRouter = require('./app/routes/skillRoute');
 
 //require('dotenv').config(); sudah dites dalam index.js, nggak bisa bekerja
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/authuser', authUserRouter);
+app.use('/skill', skillRouter);
 
 app.listen(3000, () => {
   console.log('Server berjalan di http://localhost:3000');
