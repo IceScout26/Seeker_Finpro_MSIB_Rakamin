4e9b19cea26640c151d20ddb100800c749d7a64f
IceScout26 <dhimasscout@gmail.com>
2023-11-16 00:37:58 +0700

    feat : put for edit user, get all user and by id.

---
+++ backend/app/controllers/profileUserController.js
@@ -0,0 +1,43 @@
+// profileUserController.js
+const { getAllUsers, getUserById, updateUserProfileById } = require('../models/profileUserModel');
+
+const getAllUserProfilesController = async (req, res) => {
+  try {
+    const userProfiles = await getAllUsers();
+    res.status(200).json(userProfiles);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching user profiles.");
+  }
+};
+
+const getUserProfileController = async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const userProfile = await getUserById(userId);
+    res.status(200).json(userProfile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching user profile.");
+  }
+};
+
+const updateUserProfileController = async (req, res) => {
+  const userId = req.params.userId;
+  const newData = req.body;
+
+  try {
+    const updatedProfile = await updateUserProfileById(userId, newData);
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating user profile.");
+  }
+};
+
+module.exports = {
+  getAllUserProfilesController,
+  getUserProfileController,
+  updateUserProfileController,
+};
