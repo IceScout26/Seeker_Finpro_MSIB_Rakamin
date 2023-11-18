a639c276a720ac4d90e5acae3e597cb3c4fc5ee4
Muhammad iqbal falid <214260004.mhs@stmikjabar.ac.id>
2023-11-18 14:54:28 +0700

    feat: add new import for login

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Register } from '../components/Register.jsx'
+import { Login } from '@/components/Login.jsx'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Register />
+      <Login />
     </main>
   )
 }
