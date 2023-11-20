78644a0994c5f262b6bdec37fe22b7e814718bb7
IceScout26 <dhimasscout@gmail.com>
2023-11-20 12:01:36 +0700

    chore : add noted if API can running on the body

--- backend/app/controllers/userExperienceController.js
+++ backend/app/controllers/userExperienceController.js
@@ -10,10 +10,10 @@ const getExperienceByIdController = async (req, res) => {
 
   try {
     const experience = await getExperienceById(experienceId);
-    res.status(200).json(experience);
+    res.status(200).json({ success: true, data: experience, message: "Experience fetched successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while fetching experience.");
+    res.status(500).json({ success: false, message: "An error occurred while fetching experience." });
   }
 };
 
@@ -22,10 +22,10 @@ const createExperienceController = async (req, res) => {
 
   try {
     const createdExperience = await createExperience(newExperience);
-    res.status(201).json(createdExperience);
+    res.status(201).json({ success: true, data: createdExperience, message: "Experience created successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while creating experience.");
+    res.status(500).json({ success: false, message: "An error occurred while creating experience." });
   }
 };
 
@@ -34,14 +34,11 @@ const updateExperienceByIdController = async (req, res) => {
   const updatedExperience = req.body;
 
   try {
-    const experience = await updateExperienceById(
-      experienceId,
-      updatedExperience
-    );
-    res.status(200).json(experience);
+    const experience = await updateExperienceById(experienceId, updatedExperience);
+    res.status(200).json({ success: true, data: experience, message: "Experience updated successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while updating experience.");
+    res.status(500).json({ success: false, message: "An error occurred while updating experience." });
   }
 };
 
@@ -50,10 +47,10 @@ const deleteExperienceByIdController = async (req, res) => {
 
   try {
     const deletedExperience = await deleteExperienceById(experienceId);
-    res.status(200).json(deletedExperience);
+    res.status(200).json({ success: true, data: deletedExperience, message: "Experience deleted successfully." });
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while deleting experience.");
+    res.status(500).json({ success: false, message: "An error occurred while deleting experience." });
   }
 };
 
