139840091cd0a9398711e44fbb26842178d4d886
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-11-20 13:06:37 +0700

    fix: fix directory import for icons and fix syntax

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import seekerLogo from "../../public/assets/images/sidebar/seeker-logo1.svg";
-import dashboard from "../../public/assets/images/sidebar/dashboard.svg";
-import leaderBoard from "../../public/assets/images/sidebar/leaderBoard.svg";
-import order from "../../public/assets/images/sidebar/shopping-cart.svg";
-import product from "../../public/assets/images/sidebar/product.svg";
-import salesReport from "../../public/assets/images/sidebar/salesReports.svg";
-import message from "../../public/assets/images/sidebar/message.svg";
-import settings from "../../public/assets/images/sidebar/settings.svg";
-import signOut from "../../public/assets/images/sidebar/signOut.svg";
+import seekerLogo from "../../public/assets/sidebar/seeker-logo1.svg";
+import dashboard from "../../public/assets/sidebar/dashboard.svg";
+import leaderBoard from "../../public/assets/sidebar/leaderBoard.svg";
+import order from "../../public/assets/sidebar/shopping-cart.svg";
+import product from "../../public/assets/sidebar/product.svg";
+import salesReport from "../../public/assets/sidebar/salesReports.svg";
+import message from "../../public/assets/sidebar/message.svg";
+import settings from "../../public/assets/sidebar/settings.svg";
+import signOut from "../../public/assets/sidebar/signOut.svg";
 
 export const Navigation = () => {
   return (
@@ -26,19 +26,22 @@ export const Navigation = () => {
                 <span className="sr-only">Open sidebar</span>
                 <svg
                   className="w-6 h-6"
-                  aria-hidden="true"
+                  ariaHidden="true"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    clip-rule="evenodd"
-                    fill-rule="evenodd"
+                    clipRule="evenodd"
+                    fillRule="evenodd"
                     d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
                   ></path>
                 </svg>
               </button>
-              <a href="https://www.yominovel.site" className="flex ms-2 md:me-24">
+              <a
+                href="https://www.yominovel.site"
+                className="flex ms-2 md:me-24"
+              >
                 <Image src={seekerLogo} alt="" />
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
                   Seeker
@@ -80,9 +83,22 @@ export const Navigation = () => {
                 href="#"
                 className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
               >
-                <Image src={dashboard} alt="" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
 
-                <span className="ms-3">Dashboard</span>
+                <span className="ms-3">Home</span>
               </a>
             </li>
             <li>
@@ -90,10 +106,22 @@ export const Navigation = () => {
                 href="#"
                 className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
               >
-                <Image src={leaderBoard} alt="" />
-                <span className="flex-1 ms-3 whitespace-nowrap">
-                  LeaderBoard
-                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+
+                <span className="flex-1 ms-3 whitespace-nowrap">Explore</span>
                 <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
               </a>
             </li>
@@ -102,69 +130,29 @@ export const Navigation = () => {
                 href="#"
                 className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
               >
-                <Image src={order} alt="" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+                  />
+                </svg>
 
-                <span className="flex-1 ms-3 whitespace-nowrap">Order</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
-              >
-                <Image src={product} alt="" />
-                <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
-              >
-                <Image src={salesReport} alt="" />
                 <span className="flex-1 ms-3 whitespace-nowrap">
-                  Sales Report
+                  Post a Job
                 </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
-              >
-                <Image src={message} alt="" />
-                <span className="flex-1 ms-3 whitespace-nowrap">Messages</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
-              >
-                <Image src={settings} alt="" />
-                <span className="flex-1 ms-3 whitespace-nowrap">Settings</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-white rounded-lg dark:text-white hover:bg-blue-700 dark:hover:bg-gray-700 group"
-              >
-                <Image src={signOut} alt="" />
-                <span className="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
               </a>
             </li>
           </ul>
         </div>
       </aside>
-
-      <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg">
-          <div className="space-y-8 md:grid md:grid-cols-3 md:gap-6">
-            <div className="md:col-span-1"></div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
