a110b7a4c5a3da31465ccd94f22cd97cd55549a4
IceScout26 <dhimasscout@gmail.com>
2023-11-20 11:56:39 +0700

    feat : controller userExperience

---
+++ backend/app/controllers/userExperienceController.js
@@ -0,0 +1,65 @@
+const {
+  getExperienceById,
+  createExperience,
+  updateExperienceById,
+  deleteExperienceById,
+} = require("../models/userExperienceModel");
+
+const getExperienceByIdController = async (req, res) => {
+  const experienceId = req.params.experienceId;
+
+  try {
+    const experience = await getExperienceById(experienceId);
+    res.status(200).json(experience);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching experience.");
+  }
+};
+
+const createExperienceController = async (req, res) => {
+  const newExperience = req.body;
+
+  try {
+    const createdExperience = await createExperience(newExperience);
+    res.status(201).json(createdExperience);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while creating experience.");
+  }
+};
+
+const updateExperienceByIdController = async (req, res) => {
+  const experienceId = req.params.experienceId;
+  const updatedExperience = req.body;
+
+  try {
+    const experience = await updateExperienceById(
+      experienceId,
+      updatedExperience
+    );
+    res.status(200).json(experience);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while updating experience.");
+  }
+};
+
+const deleteExperienceByIdController = async (req, res) => {
+  const experienceId = req.params.experienceId;
+
+  try {
+    const deletedExperience = await deleteExperienceById(experienceId);
+    res.status(200).json(deletedExperience);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while deleting experience.");
+  }
+};
+
+module.exports = {
+  getExperienceByIdController,
+  createExperienceController,
+  updateExperienceByIdController,
+  deleteExperienceByIdController,
+};
