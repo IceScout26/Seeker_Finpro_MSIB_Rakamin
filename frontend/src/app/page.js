9e4f95c009ce3e4ba32d49e5acca3599e32dc849
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-11-20 13:00:25 +0700

    feat: add new import for explore and fix syntax

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 // import { Register } from '../components/Register.jsx'
 import { Register } from './register/page.jsx'
+import Explore from './company/explore/page.jsx'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Register />
+      <Explore />
     </main>
   )
 }
