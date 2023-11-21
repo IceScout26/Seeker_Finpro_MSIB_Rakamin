91e606dc1be0bdaaca6399690d5f34586043be52
IceScout26 <dhimasscout@gmail.com>
2023-11-21 12:31:34 +0700

    feat : router for profile user

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,7 @@ const authUserRouter = require('./app/routes/authUserRoute');
 const authCompanyRouter = require('./app/routes/authCompanyRoute');
 const skillRouter = require('./app/routes/skillRoute');
 const profileUserRouter = require('./app/routes/profileUserRoute');
-// const jobPostingCompanyRouter = require('./app/routes/jobPostingCompanyRoute');
-// const jobPostingUserRouter = require('./app/routes/jobPostingUserRoute');
+const userPhotoRouter = require('./app/routes/userPhotoRoute');
 const userExperienceRouter = require('./app/routes/userExperienceRoute');
 
 const app = express();
@@ -17,10 +16,8 @@ app.use('/authuser', authUserRouter);
 app.use('/authcompany', authCompanyRouter);
 app.use('/skill', skillRouter);
 app.use('/profileusers', profileUserRouter);
+app.use('/profilephotos', userPhotoRouter);
 app.use('/experiences', userExperienceRouter);
-
-// app.use('/jobs/company', jobPostingCompanyRouter);
-// app.use('/jobs/user', jobPostingUserRouter);
 
 app.listen(3000, () => {
   console.log('Server berjalan di http://localhost:3000');
