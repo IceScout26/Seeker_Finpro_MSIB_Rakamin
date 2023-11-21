159996ad0d9873d1b3358ec80dd8e5c1e5a2b103
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-11-21 15:56:44 +0700

    feat: add new import for Experiences and School

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,12 +2,16 @@ import Image from 'next/image'
 // import { Register } from '../components/Register.jsx'
 import { Register } from './register/page.jsx'
 import Explore from './company/explore/page.jsx'
+import Experiences from './user/experience/page.jsx'
+import School from './user/school/page.jsx'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Register />
       <Explore />
+      <Experiences />
+      <School />
     </main>
   )
 }
