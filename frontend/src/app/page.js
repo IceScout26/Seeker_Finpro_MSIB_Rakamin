5e6ddb0582393c9baf2b5bec589bebf20f8c0ba6
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-11-29 23:43:29 +0700

    feat: add new import for education, detailUser, companyExplore, userExplore, companyProfile, profile

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,26 +1,32 @@
 import Image from 'next/image'
 // import { Register } from '../components/Register.jsx'
 import { Register } from './register/page.jsx'
-import Explore from './company/explore/page.jsx'
+import companyExplore from './company/explore/page.jsx'
 import Experiences from './user/experience/page.jsx'
-import School from './user/school/page.jsx'
-import Detail from './company/detail/page.jsx'
+import Education from './user/education/page.jsx'
+import detailUser from './company/detail/page.jsx'
 import Login from './login/page.jsx'
 import UserHome from './user/home/page.jsx'
-import CompanyHome from './company/home/page.jsx'
+import companyHome from './company/home/page.jsx'
+import userExplore from './user/explore/page.jsx'
+import companyProfile from './company/profile/page.jsx'
+import Profile from './user/profile/page.jsx'
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Register />
-      <Explore />
+      <companyExplore />
       <Experiences />
-      <School />
-      <Detail />
+      <Education />
+      <detailUser />
       <Login />
       <UserHome />
-      <CompanyHome />
+      <companyHome />
+      <userExplore />
+      <companyProfile />
+      <Profile />
     </main>
   )
 }
