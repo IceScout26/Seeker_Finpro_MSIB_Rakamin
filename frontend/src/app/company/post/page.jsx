f3017da682810de3e3a79db8be6f7625a08d1dc3
GerySalutz <geriislami25@gmail.com>
2023-11-30 14:07:44 +0700

    fix: update syntax input for post

--- frontend/src/app/company/post/page.jsx
+++ frontend/src/app/company/post/page.jsx
@@ -16,24 +16,26 @@ export default function CompanyJob() {
             <div className="pt-5 text-2xl font-semibold text-gray-800">
               Job Description
             </div>
-            <input
-              name="job-description"
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full h-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
+            <div className="mt-2">
+              <textarea
+                name="description"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full h-32 p-2.5 "
+                placeholder="Description"
+                required
+              ></textarea>
+            </div>
             <div className="pt-5 text-2xl font-semibold text-gray-800">
               Expertise
             </div>
-            <input
-              name="expertise"
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full h-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=""
-              required
-            />
-            <div className="pt-5 text-2xl font-semibold text-gray-800">
+            <div className="mt-2">
+              <textarea
+                name="expertise"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full h-32 p-2.5 "
+                placeholder="Expertise"
+                required
+              ></textarea>
+            </div>
+            {/* <div className="pt-5 text-2xl font-semibold text-gray-800">
               Salary
             </div>
             <input
@@ -52,7 +54,44 @@ export default function CompanyJob() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Judul"
               required
-            />
+            /> */}
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-2xl font-semibold text-gray-800 leading-6 "
+                >
+                  Salary
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="last-name"
+                  className="block text-2xl font-semibold text-gray-800 leading-6"
+                >
+                  Due
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="flex justify-center pt-10">
@@ -67,4 +106,4 @@ export default function CompanyJob() {
       </div>
     </div>
   );
-};
+}
